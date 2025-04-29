748a0dccb1c0b15fc0ce43e03ccba7011512f90d
Yae0311 <jason.slasherrblx@gmail.com>
2025-04-29 19:02:07 +0800

    Update helper.php

--- helper.php
+++ helper.php
@@ -1,9 +1,9 @@
 <?php
 // Establish a database connection
-$host = "localhost";
-$username = "root";
-$password = "";
-$db_name = "florist";
+$host = "project-db.cufzmwkjncuf.us-east-1.rds.amazonaws.com";
+$username = "main";
+$password = "project-password";
+$db_name = "project";
 
 $con = mysqli_connect($host, $username, $password, $db_name);
 if (mysqli_connect_errno()) {
